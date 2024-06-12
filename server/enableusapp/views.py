ebc9247c99c32e997474761768330800292e8451
zulfa Ramadhan <zulfar436@gmail.com>
2024-06-12 21:49:54 +0300

    made changes to the view of single profile

--- server/enableusapp/views.py
+++ server/enableusapp/views.py
@@ -115,7 +115,21 @@ class SingleProfileView(APIView):
         userprofile=self.getById(id)
         serializer=UserProfileSerializer(userprofile)
         return Response(serializer.data)
-     
+
+
+    def put(self,request,id,format=None):
+        userprofile=self.getById(id)
+        serializer=UserProfileSerializer (userprofile,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)  
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+
+
+    def delete(self,request,id,format=None):
+        userprofile=self.getById(id)
+        userprofile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)    
 
            
 
